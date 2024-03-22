66e4e898e8873309143f1140b1c89b0c8c90bdc0
“Am91T” <tiouk.amine@gmail.com>
2024-03-22 15:34:27 +0000

    Done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,7 @@
-package { 'flask':
-  ensure => '2.1.0',
+# Puppet manifest to install Flask from pip3
+
+# Ensure the Flask package is installed and at version 2.1.0
+package { 'python3-flask':
+  ensure   => '2.1.0',
   provider => 'pip3',
 }
