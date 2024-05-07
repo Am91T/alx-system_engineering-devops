7c00591ab9c8ab81fa96619b76ceac0ea6dc1ae1
“Am91T” <tiouk.amine@gmail.com>
2024-05-07 23:07:32 +0100

    To Know how many subs in subreddit?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API for the number of subscribers of a given subreddit.
+
+    Args:
+        subreddit: The name of the subreddit to query.
+
+    Returns:
+        The number of subscribers (integer)
+        or 0 if the subreddit is invalid or an error occurs.
+    """
+
+    from requests import get
+
+    headers = {'User-Agent': 'my_custom_user_agent'}
+    response = get(f"https://www.reddit.com/r/{subreddit}/about.json",
+                   headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data', {}).get('subscribers', 0)
+    else:
+        return 0
