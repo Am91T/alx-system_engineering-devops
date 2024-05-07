5dfd358a3603fd380f68fd008f502ef42b174436
“Am91T” <tiouk.amine@gmail.com>
2024-05-07 23:18:35 +0100

    To Know how many subs in subreddit?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,32 +2,22 @@
 '''
     this module contains the function number_of_subscribers
 '''
-
-from requests import get
+import requests
 from sys import argv
 
+
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API for the number of subscribers of a given subreddit.
-
-    Args:
-        subreddit: The name of the subreddit to query.
-
-    Returns:
-        The number of subscribers (integer)
-        or 0 if the subreddit is invalid or an error occurs.
-    """
-
-
-    headers = {'User-Agent': 'my_custom_user_agent'}
-    response = get(f"https://www.reddit.com/r/{subreddit}/about.json",
-                   headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()
-        return data.get('data', {}).get('subscribers', 0)
-    else:
+    '''
+        returns the number of subscribers for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/about.json'
+                       .format(subreddit), headers=user).json()
+    try:
+        return url.get('data').get('subscribers')
+    except Exception:
         return 0
+
 
 if __name__ == "__main__":
     number_of_subscribers(argv[1])
